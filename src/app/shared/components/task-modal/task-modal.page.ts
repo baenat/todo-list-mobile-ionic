b02a1246c5d35ea0e89ce7da2648e0d9b91510e0
Baenat <bae.julian10@gmail.com>
2025-10-24 22:51:52 -0500

    feat: Añade componente modal donde se añaden y editan las tareas

---
+++ src/app/shared/components/task-modal/task-modal.page.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonButton, IonInput, IonModal, IonSelect, IonSelectOption, } from '@ionic/angular/standalone';
+import { Category } from 'src/app/domain/entities/category.entity';
+import { Task } from 'src/app/domain/entities/task.entity';
+
+@Component({
+  selector: 'task-modal',
+  templateUrl: './task-modal.page.html',
+  styleUrls: ['./task-modal.page.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonModal, IonButton, IonInput, IonSelect, IonSelectOption]
+})
+export class TaskModalPage {
+
+  task = input<Task | null>();
+  categories = input.required<Category[]>();
+
+  isOpen = input.required<boolean>();
+  title = input<string>('Nueva Tarea');
+
+  saveTask = output<Task>();
+  closeModal = output<void>();
+
+  nameTask = signal<string>('');
+  categoryTask = signal<string>('');
+
+  constructor() { }
+
+  onInputName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.nameTask.set(String(input.value ?? ''));
+  }
+
+  onCategoryChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.categoryTask.set(String(input.value ?? ''));
+  }
+
+  save() {
+    const task = this.nameTask();
+    const category = this.categoryTask();
+    if (task && category) {
+      const savedTask: Task = {
+        id: this.task()?.id || crypto.randomUUID(),
+        title: task,
+        categoryId: category,
+        completed: this.task()?.completed || false,
+        createdAt: new Date(),
+      };
+      this.saveTask.emit(savedTask);
+      this.closeModal.emit();
+    }
+  }
+
+}
