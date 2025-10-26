42160b51947147e24dd16984e763846a91677589
Baenat <bae.julian10@gmail.com>
2025-10-26 17:54:00 -0500

    refactor: Optimiza el component de lista las categorias para filtrado, enviado con input las categorias

--- src/app/shared/components/category-selector/category-selector.page.ts
+++ src/app/shared/components/category-selector/category-selector.page.ts
@@ -1,9 +1,10 @@
-import { Component, effect, OnInit, output, signal } from '@angular/core';
+import { Component, effect, input, OnInit, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonChip } from "@ionic/angular/standalone";
 import { CategoryEntity } from 'src/app/domain/entities/category.entity';
 import { ArrayCategoryRepositoryImpl } from '@data/repositories/array/array-category.repository.impl';
+import { GetCategoryUseCase } from '@domain/usecases/category/get-category.usecase';
 
 @Component({
   selector: 'category-selector',
@@ -12,25 +13,15 @@ import { ArrayCategoryRepositoryImpl } from '@data/repositories/array/array-cate
   standalone: true,
   imports: [CommonModule, FormsModule, IonChip]
 })
-export class CategorySelectorPage implements OnInit {
+export class CategorySelectorPage {
 
-  categories = signal<CategoryEntity[]>([]);
+  categories = input<CategoryEntity[]>([]);
   selectedCategory = signal<string>('');
   category = output<string>();
 
-  constructor(
-    private categoryRepo: ArrayCategoryRepositoryImpl
-  ) { }
+  constructor() { }
 
   categorySelectedEffect = effect(() => {
     this.category.emit(this.selectedCategory());
-  })
-
-  ngOnInit() {
-    this.loadData();
-  }
-
-  async loadData() {
-    this.categories.set(await this.categoryRepo.getCategories());
-  }
+  });
 }
