f021d6ebe3be172fe7f6f7833489ef3bfc6a0233
Baenat <bae.julian10@gmail.com>
2025-10-27 08:15:25 -0500

    feat: Implementa repositorios Firebase con logica de consumo

--- src/app/data/repositories/firebase/firebase-category.repository.impl.ts
+++ src/app/data/repositories/firebase/firebase-category.repository.impl.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
-import { TaskEntity } from 'src/app/domain/entities/task.entity';
-import { TaskRepository } from 'src/app/domain/repositories/task.repository';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { CategoryEntity } from '@domain/entities/category.entity';
+import { CategoryRepository } from '@domain/repositories/category.repository';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FirebaseCategoryRepositoryImpl extends TaskRepository {
+export class FirebaseCategoryRepositoryImpl extends CategoryRepository {
 
+  constructor(
+    private firestore: Firestore,
+  ) {
+    super();
+  }
 
+  async getCategories(): Promise<CategoryEntity[]> {
+    const categoriesRef = collection(this.firestore, 'category');
+    return firstValueFrom(collectionData(categoriesRef, { idField: 'id' }) as Observable<CategoryEntity[]>);
+  }
 
-  async getTasks(): Promise<TaskEntity[]> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async addTask(task: TaskEntity): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async updateTask(task: TaskEntity): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async deleteTask(id: string): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
+  async addCategory(category: CategoryEntity): Promise<void> {
+    const categoriesRef = collection(this.firestore, 'category');
+    addDoc(categoriesRef, category);
+  }
+
+  async updateCategory(category: CategoryEntity): Promise<void> {
+    const categoriesRef = doc(this.firestore, `category/${category.id}`)
+    updateDoc(categoriesRef, { ...category });
+  }
+
+  async deleteCategory(id: string): Promise<void> {
+    const categoriesRef = doc(this.firestore, `category/${id}`);
+    deleteDoc(categoriesRef);
+  }
 }
+
--- src/app/data/repositories/firebase/firebase-task.repository.impl.ts
+++ src/app/data/repositories/firebase/firebase-task.repository.impl.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { firstValueFrom, Observable } from 'rxjs';
 import { TaskEntity } from 'src/app/domain/entities/task.entity';
 import { TaskRepository } from 'src/app/domain/repositories/task.repository';
 
@@ -7,10 +9,29 @@ import { TaskRepository } from 'src/app/domain/repositories/task.repository';
 })
 export class FirebaseTaskRepositoryImpl extends TaskRepository {
 
+  constructor(
+    private firestore: Firestore,
+  ) {
+    super();
+  }
 
+  async getTasks(): Promise<TaskEntity[]> {
+    const tasksRef = collection(this.firestore, 'task');
+    return firstValueFrom(collectionData(tasksRef, { idField: 'id' }) as Observable<TaskEntity[]>);
+  }
 
-  async getTasks(): Promise<TaskEntity[]> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async addTask(task: TaskEntity): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async updateTask(task: TaskEntity): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
-  async deleteTask(id: string): Promise<void> { throw new Error('FirebaseTaskRepositoryImpl: Not implemented'); }
+  async addTask(task: TaskEntity): Promise<void> {
+    const tasksRef = collection(this.firestore, 'task');
+    addDoc(tasksRef, task);
+  }
+
+  async updateTask(task: TaskEntity): Promise<void> {
+    const tasksRef = doc(this.firestore, `task/${task.id}`)
+    updateDoc(tasksRef, { ...task });
+  }
+
+  async deleteTask(id: string): Promise<void> {
+    const noteDocRef = doc(this.firestore, `task/${id}`);
+    deleteDoc(noteDocRef);
+  }
 }
